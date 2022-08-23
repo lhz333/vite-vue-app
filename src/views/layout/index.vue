e35ee565e5272d7452b8950bc60f23d6335cac7b
Jenny <18839970925@163.com>
2022-08-23 11:15:52 +0800

    feat: layout

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -16,12 +16,20 @@ login()
 </script>
 
 <template>
-  <h1 class="title">首页</h1>
-  <SvgIcon icon-class="vue" />
-  <SvgIcon icon-class="crown" />
-  <div>count: {{ user.count }}</div>
-  <div>double: {{ user.double }}</div>
-  <el-button @click="user.increment">add+</el-button>
+  <el-container>
+    <el-header>Header</el-header>
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-main>
+        <h1 class="title">首页</h1>
+        <SvgIcon icon-class="vue" />
+        <SvgIcon icon-class="crown" />
+        <div>count: {{ user.count }}</div>
+        <div>double: {{ user.double }}</div>
+        <el-button @click="user.increment">add+</el-button>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <style scoped lang="scss">
